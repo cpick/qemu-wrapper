f2e064a769525923e677c31ff2c702ebd0941e6c
Chris Pick <chris@chrispick.com>
2024-10-12 11:02:36 -0400

    Extract `set_tty_attributes()` method
    Prepare to block `SIGTTOU` during `tcsetattr()` without repeating code.

--- src/raw_guard/mod.rs
+++ src/raw_guard/mod.rs
@@ -2,7 +2,7 @@ use anyhow::{Context, Error, Result};
 use nix::errno::Errno;
 use nix::sys::termios::{cfmakeraw, tcgetattr, tcsetattr, LocalFlags, SetArg, Termios};
 use std::io::stdin;
-use std::os::fd::AsRawFd as _;
+use std::os::fd::{AsRawFd as _, RawFd};
 
 pub struct RawGuard {
     termios: Option<Termios>,
@@ -21,19 +21,29 @@ impl RawGuard {
             let mut termios_raw = termios.clone();
             cfmakeraw(&mut termios_raw);
             termios_raw.local_flags |= termios.local_flags & LocalFlags::ISIG;
-            tcsetattr(stdin, SetArg::TCSANOW, &termios_raw).context("tcsetattr")?;
+            Self::set_tty_attributes(stdin, &termios_raw).context("set_tty_attributes")?;
         }
 
         Ok(Self {
             termios: Some(termios),
         })
     }
+
+    fn set_tty_attributes(fd: RawFd, termios: &Termios) -> Result<()> {
+        tcsetattr(fd, SetArg::TCSANOW, termios).context("tcsetattr")?;
+        Ok(())
+    }
 }
 
 impl Drop for RawGuard {
     fn drop(&mut self) {
         if let Some(termios) = &self.termios {
-            let _ = tcsetattr(stdin().as_raw_fd(), SetArg::TCSANOW, termios);
+            match Self::set_tty_attributes(stdin().as_raw_fd(), termios)
+                .context("set_tty_attributes")
+            {
+                Ok(()) => (),
+                Err(error) => eprintln!("Error: {error:?}"),
+            }
         }
     }
 }
