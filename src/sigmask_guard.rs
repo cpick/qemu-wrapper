5acba9c1c903fc5c24861c46726869f18a451f4c
Chris Pick <chris@chrispick.com>
2024-10-15 19:05:21 -0400

    Use `Signal::thread_*()` for (un)blocking signals
    Make the operations a bit clearer.

--- src/sigmask_guard.rs
+++ src/sigmask_guard.rs
@@ -1,6 +1,6 @@
 use anyhow::{Context, Result};
-use nix::sys::signal::{pthread_sigmask, SigSet, SigmaskHow};
-use std::io::{Error as IoError, Result as IoResult};
+use nix::sys::signal::{SigSet, SigmaskHow};
+use std::io::Result as IoResult;
 
 pub struct SigmaskGuard {
     previous: Option<SigSet>,
@@ -8,17 +8,20 @@ pub struct SigmaskGuard {
 
 impl SigmaskGuard {
     pub fn new(block: &SigSet) -> Result<Self> {
-        let mut previous = SigSet::empty();
-        pthread_sigmask(SigmaskHow::SIG_BLOCK, Some(block), Some(&mut previous))
-            .context("block pthread_sigmask")?;
         Ok(Self {
-            previous: Some(previous),
+            previous: Some(
+                block
+                    .thread_swap_mask(SigmaskHow::SIG_BLOCK)
+                    .context("thread swap mask block")?,
+            ),
         })
     }
 
     pub fn unblock(&mut self) -> IoResult<()> {
-        pthread_sigmask(SigmaskHow::SIG_SETMASK, self.previous.take().as_ref(), None)
-            .map_err(|error| IoError::from_raw_os_error(error as i32))
+        if let Some(previous) = self.previous.take() {
+            previous.thread_set_mask()?;
+        }
+        Ok(())
     }
 }
 
