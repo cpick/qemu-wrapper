6c04209c2755938d3f5d3775116ec4b2799f5fe0
Chris Pick <chris@chrispick.com>
2025-04-26 22:07:04 -0400

    Add `qemu-plugin` example code
    From `qemu-plugin`s
    [documentation](https://github.com/novafacing/qemu-rs/blob/v9.0.0-v0/qemu-plugin/README.md#example)

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,40 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use anyhow::Result;
+use ctor::ctor;
+use qemu_plugin::{
+    PluginId, TranslationBlock,
+    plugin::{HasCallbacks, PLUGIN, Plugin, Register},
+};
+use std::sync::Mutex;
+
+struct TinyTrace {}
+
+impl Register for TinyTrace {}
+
+impl HasCallbacks for TinyTrace {
+    fn on_translation_block_translate(
+        &mut self,
+        _id: PluginId,
+        tb: TranslationBlock,
+    ) -> Result<()> {
+        tb.instructions().enumerate().try_for_each(|(idx, insn)| {
+            if idx == 0 {
+                println!("====TB: {:08x}", insn.vaddr());
+            }
+
+            println!("{:08x}: {}", insn.vaddr(), insn.disas()?);
+            Ok::<(), anyhow::Error>(())
+        })?;
+
+        Ok(())
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Plugin for TinyTrace {}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[ctor]
+fn init() {
+    PLUGIN
+        .set(Mutex::new(Box::new(TinyTrace {})))
+        .map_err(|_| anyhow::anyhow!("Failed to set plugin"))
+        .expect("Failed to set plugin");
 }
