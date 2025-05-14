73da8ffa2bf2022d8956f406420b8e3660b330a6
Chris Pick <chris@chrispick.com>
2025-05-14 14:32:17 -0400

    Extract noop target from .cargo/config.toml
    Ensure it only has to be configured in one place.

--- tests/main.rs
+++ tests/main.rs
@@ -1,16 +1,36 @@
-use std::path::{Path, PathBuf};
+use std::{env, fs, path::PathBuf};
 
+use serde::Deserialize;
 use test_binary::TestBinary;
+
+#[derive(Deserialize)]
+struct Build {
+    target: String,
+}
+
+#[derive(Deserialize)]
+struct Config {
+    build: Build,
+}
 
 #[test]
 fn main() {
-    let directory = Path::new("testbins");
     let name = "noop";
-    let _noop = TestBinary::relative_to_parent(
-        name,
-        &PathBuf::from_iter([directory, name.as_ref(), "Cargo.toml".as_ref()]),
-    )
-    .with_target("x86_64-unknown-uefi")
-    .build()
-    .expect("build test binary noop");
+    let directory = PathBuf::from_iter(["testbins", &name]);
+
+    let config = {
+        let cargo_manifest_dir =
+            env::var_os("CARGO_MANIFEST_DIR").expect("env var os cargo manifest dir");
+
+        let mut config = PathBuf::from(cargo_manifest_dir);
+        config.extend([&*directory, ".cargo".as_ref(), "config.toml".as_ref()]);
+
+        let config = fs::read_to_string(config).expect("read to string config");
+        toml::from_str::<Config>(&config).expect("toml from str config")
+    };
+
+    let _noop = TestBinary::relative_to_parent(name, &directory.join("Cargo.toml"))
+        .with_target(&config.build.target)
+        .build()
+        .expect("build test binary noop");
 }
