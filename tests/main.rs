ff9f073226d292e789d743e0b6094d738df4a3fc
Chris Pick <chris@chrispick.com>
2025-05-14 15:13:53 -0400

    Execute noop in test

--- tests/main.rs
+++ tests/main.rs
@@ -1,4 +1,8 @@
-use std::{env, fs, path::PathBuf};
+use std::{
+    env, fs,
+    path::PathBuf,
+    process::{self, Command},
+};
 
 use serde::Deserialize;
 use test_binary::TestBinary;
@@ -16,21 +20,64 @@ struct Config {
 #[test]
 fn main() {
     let name = "noop";
-    let directory = PathBuf::from_iter(["testbins", &name]);
+    let source_dir = PathBuf::from_iter(["testbins", &name]);
 
     let config = {
-        let cargo_manifest_dir =
-            env::var_os("CARGO_MANIFEST_DIR").expect("env var os cargo manifest dir");
-
-        let mut config = PathBuf::from(cargo_manifest_dir);
-        config.extend([&*directory, ".cargo".as_ref(), "config.toml".as_ref()]);
+        let mut config = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        config.extend([&*source_dir, ".cargo".as_ref(), "config.toml".as_ref()]);
 
         let config = fs::read_to_string(config).expect("read to string config");
         toml::from_str::<Config>(&config).expect("toml from str config")
     };
 
-    let _noop = TestBinary::relative_to_parent(name, &directory.join("Cargo.toml"))
+    let noop = TestBinary::relative_to_parent(name, &source_dir.join("Cargo.toml"))
         .with_target(&config.build.target)
         .build()
         .expect("build test binary noop");
+
+    let esp_dir = scopeguard::guard(
+        PathBuf::from_iter([
+            env!("CARGO_TARGET_TMPDIR"),
+            &format!("esp-{}", process::id()),
+        ]),
+        |dir| fs::remove_dir_all(dir).expect("remove dir all"),
+    );
+    // FIXME: warn and remove if already exists
+
+    {
+        let mut boot = esp_dir.clone();
+        boot.extend(["efi", "boot"]);
+        fs::create_dir_all(&boot).expect("create dir all boot");
+        boot.push("bootx64.efi");
+        fs::hard_link(noop, boot).expect("hard link noop boot");
+    }
+
+    const QEMU: &str = "qemu-system-x86_64";
+    let mut ovmf_dir = which::which(QEMU).expect("which qemu");
+    ovmf_dir.pop(); // filename
+    ovmf_dir.pop(); // bin
+    ovmf_dir.extend(["share", "qemu"]);
+    let ovmf_dir = ovmf_dir.to_str().expect("ovmf dir to str");
+
+    let status = Command::new(QEMU)
+        .args([
+            "-drive",
+            &format!("if=pflash,format=raw,readonly=on,file={ovmf_dir}/edk2-x86_64-code.fd"),
+            "-drive",
+            &format!("if=pflash,format=raw,readonly=on,file={ovmf_dir}/edk2-i386-vars.fd"),
+            "-drive",
+            &format!(
+                "format=raw,file=fat:rw:{}",
+                esp_dir.to_str().expect("esp dir to str")
+            ),
+            "-device",
+            "isa-debug-exit,iobase=0xf4,iosize=0x01",
+            "-nic",
+            "none",
+            "-nographic",
+            "-no-reboot",
+        ])
+        .status()
+        .expect("command noop status");
+    assert_eq!(Some(7), status.code());
 }
